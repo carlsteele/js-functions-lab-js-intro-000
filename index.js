9f2dbe9a30d9b3b3d424d926fa41431bc6e0c549
Carl Steele <casteeleu@gmail.com>
2018-11-19 04:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${bobby}!`
+}
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${Hannakuah
+}
